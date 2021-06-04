7f5b7be471ec3691b5f7d83ef69a39c02daf93b8
Baldip Bhuller <Bbhuller1839@conestogac.on.ca>
2021-06-04 11:34:53 -0400

    Done calculator functionality

--- ConsoleCalculator/ConsoleCalculator.cs
+++ ConsoleCalculator/ConsoleCalculator.cs
@@ -2,6 +2,8 @@
 
 namespace ConsoleCalculator
 {
+    // Baldip Singh Bhuller
+
     public class ConsoleCalculator
     {
         private decimal firstNumber;
@@ -16,36 +18,86 @@ namespace ConsoleCalculator
         public decimal FirstNumber { get => firstNumber; set => firstNumber = value; }
         public decimal SecondNumber { get => secondNumber; set => secondNumber = value; }
 
-        public static void RunConsole()
+        // Addition method rounded to 3 decimal places
+        public decimal Addition(decimal firstNumber, decimal secondNumber)
+        {
+            return Math.Round(firstNumber + secondNumber, 3);
+        }
+
+        // Subtraction method rounded to 3 decimal places
+        public decimal Subtraction(decimal firstNumber, decimal secondNumber)
+        {
+            return Math.Round(firstNumber - secondNumber, 3);
+        }
+
+        // Multiplication method rounded to 3 decimal places
+        public decimal Multiplication(decimal firstNumber, decimal secondNumber)
+        {
+            return Math.Round(firstNumber * secondNumber, 3);
+        }
+
+        // Division method rounded to 3 decimal places
+        public decimal Division(decimal firstNumber, decimal secondNumber)
+        {
+            return Math.Round(firstNumber / secondNumber, 3);
+        }
+
+        public static void RunConsoleView()
         {
             string operationInput, firstNumber, secondNumber;
+            decimal result;
 
             Console.WriteLine("Console Calculator");
             Console.Write("Choose operation: \n" +
                            "+ for Addition \n" +
                            "- for Subtraction \n" +
                            "* for Multiplication \n" +
-                           "/ for Division \n");
+                           "/ for Division \n" +
+                           "Type input here:" );
 
+            //Get input operation
             operationInput = Console.ReadLine();
 
             ConsoleCalculator consoleCalculator = new ConsoleCalculator();
 
+            //Get input first number
             Console.Write("Enter first number:");
             firstNumber = Console.ReadLine();
             consoleCalculator.FirstNumber = decimal.Parse(firstNumber);
 
+            //Get input second number
             Console.Write("Enter second number:");
             secondNumber = Console.ReadLine();
             consoleCalculator.SecondNumber = decimal.Parse(secondNumber);
 
-            Console.WriteLine("First number: {0}", consoleCalculator.FirstNumber);
-            Console.WriteLine("Second number: {0}", consoleCalculator.SecondNumber);
+            //Check operation
+            switch (operationInput)
+            {
+                case "+": 
+                    result = consoleCalculator.Addition(consoleCalculator.FirstNumber, consoleCalculator.SecondNumber);
+                    break;
+                case "-":
+                    result = consoleCalculator.Subtraction(consoleCalculator.FirstNumber, consoleCalculator.SecondNumber);
+                    break;
+                case "*":
+                    result = consoleCalculator.Multiplication(consoleCalculator.FirstNumber, consoleCalculator.SecondNumber);
+                    break;
+                case "/":
+                    result = consoleCalculator.Division(consoleCalculator.FirstNumber, consoleCalculator.SecondNumber);
+                    break;
+                default:
+                    Console.WriteLine("Incorrect operation input");
+                    result = 0;
+                    break;
+            }
+
+            //Show result
+            Console.Write("Result: {0}", result);
         }
 
         public static void Main(string[] args)
         {
-            RunConsole();
+            RunConsoleView();
         }
     }
 }
